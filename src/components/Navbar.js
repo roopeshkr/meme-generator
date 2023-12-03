464bd6f01a0f32ba4cb5abcc637f1f41c025d894
Roopesh Rokade <04012001roopesh@gmail.com>
2023-12-03 11:35:47 +0530

    meme generator

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <header className="header">
+      <img src="/troll-face.png" alt="logo" className="header--image"></img>
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
+
+export default Navbar;
